2df4e6e9171ee0c0881f7eb1896e1baa6d0cf800
YeeNomSki <smj9839@gmail.com>
2019-01-19 21:38:40 +0100

    page now set as main instead of accessing from crypto folder

--- ccgains_project/ccgains_project/urls.py
+++ ccgains_project/ccgains_project/urls.py
@@ -18,8 +18,8 @@ from django.urls import path, include
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('crypto/', include('crypto.urls')),
+    path('', include('crypto.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('crypto/', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('', TemplateView.as_view(template_name='crypto/index.html'), name='index'),
 ]
