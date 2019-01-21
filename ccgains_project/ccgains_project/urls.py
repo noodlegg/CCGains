876f3af3f696e7cd850adb1b23daf17587b765e0
YeeNomSki <smj9839@gmail.com>
2019-01-21 16:35:31 +0100

    deleted duplicate line in urls.py

--- ccgains_project/ccgains_project/urls.py
+++ ccgains_project/ccgains_project/urls.py
@@ -21,6 +21,5 @@ urlpatterns = [
     path('crypto/', include('crypto.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')), 
-    path('accounts/', include('crypto.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
